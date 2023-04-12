58398595705bfb66327583802aed99c1f4c2310f
John Patrick <jpattrick538@gmail.com>
2023-04-12 21:31:36 +0300

    added use effect for count

--- Challenge1/Client/chasing_the_clicks/src/App.js
+++ Challenge1/Client/chasing_the_clicks/src/App.js
@@ -16,14 +16,14 @@ import DOMPurify from "dompurify";
 function App() {
   const [geoInfo, setGeoInfo] = useState([]);
   const [intClicks, setInt] = useState(null);
-  if (intClicks === null) {
+  useEffect(()=>{
     fetch("https://my-clicks-se-apis.vercel.app/getclicks")
-      .then((res) => res.json())
-      .then((data) => {
-        setInt(data[0].clicks);
-      })
-      .catch((err) => console.log(err));
-  }
+    .then((res) => res.json())
+    .then((data) => {
+      setInt(data[0].clicks);
+    })
+    .catch((err) => console.log(err));
+  },[])
   const handleClicks = () => {
     fetch("https://my-clicks-se-apis.vercel.app/updateclicks")
       .then((res) => res.json())
