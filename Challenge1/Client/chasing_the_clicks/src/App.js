c8502b38d12f806ddeeb775866a8ed46ab50a251
John Patrick <jpattrick538@gmail.com>
2023-04-15 12:17:06 +0300

    added loading effect to counter

--- Challenge1/Client/chasing_the_clicks/src/App.js
+++ Challenge1/Client/chasing_the_clicks/src/App.js
@@ -12,30 +12,34 @@ import {
 import "@aws-amplify/ui-react/styles.css";
 import GeoTable from "./components/tabledata";
 import DOMPurify from "dompurify";
-
+import loadingBar from "./components/loading";
 function App() {
   const [geoInfo, setGeoInfo] = useState([]);
   const [intClicks, setInt] = useState();
-  if(intClicks === NaN){
-    setInt("Loading Counts...")
-  }
+  const [loaded, setIsLoaded] = useState(false);
+
   useEffect(() => {
     fetch("https://my-clicks-se-apis.vercel.app/getclicks")
       .then((res) => res.json())
       .then((data) => {
         setInt(data[0].clicks);
+        setTimeout(() => {
+          setIsLoaded(true);
+        }, 1000);
       })
       .catch((err) => console.log(err));
   }, []);
 
   const handleClicks = () => {
-
-    setInt(prev=>{
-      return ++prev
-    })
-    fetch("https://my-clicks-se-apis.vercel.app/updateclicks").catch((err) => {console.log(err); setInt(prev=>{
-      return --prev
-    })});
+    setInt((prev) => {
+      return ++prev;
+    });
+    fetch("https://my-clicks-se-apis.vercel.app/updateclicks").catch((err) => {
+      console.log(err);
+      setInt((prev) => {
+        return --prev;
+      });
+    });
   };
 
   let checkGeoData = () => {
@@ -57,12 +61,15 @@ function App() {
       </nav>
       <section className="main">
         <div className="counter">
+         {!loaded ? loadingBar("spinningBubbles","Black") :<div>
           <h1>{intClicks}</h1>
-          <Button onClick={handleClicks}>Click Me</Button>
+          <Button  onClick={handleClicks}>Click Me</Button></div>
+        }
         </div>
+        
         <div className="geostat">
           <div>
-            <hr />
+                        <hr />
             <h3>
               Clicks By Region{" "}
               <Button className="reveal" size="small" onClick={checkGeoData}>
